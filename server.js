0eb0da56eaa77693b1ecec72c5fef942e74e0e03
Nick Kidder <nickidder@yahoo.com>
2019-09-22 15:53:49 -0700

    added html and css

--- server.js
+++ server.js
@@ -45,30 +45,35 @@ app.get("/scrape", function (req, res) {
         res.send("Scrape Complete");
     });
 });
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
     db.Article.find({})
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
-    .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                note: dbNote._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
-  
+});
